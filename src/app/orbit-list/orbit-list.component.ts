0ea5936c685595e27794cab8c8561f67a6d22cf0
Angie Zarinelli <angie.zarinelli@gmail.com>
2022-07-26 20:16:14 -0500

    bonus

--- src/app/orbit-list/orbit-list.component.ts
+++ src/app/orbit-list/orbit-list.component.ts
@@ -26,5 +26,9 @@ export class OrbitListComponent implements OnInit {
 			return 0;
 		});
 	}
-
+	zebraStripes(arrayItem): boolean {
+		let index: number = this.satellites.indexOf
+		(arrayItem);
+		return index % 2 === 0;
+	}
 }
